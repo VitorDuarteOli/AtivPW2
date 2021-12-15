ee1c3ec7f87682db24eb11e0458c7b3df3d76f10
VitorDuarteOli <96184821+VitorDuarteOli@users.noreply.github.com>
2021-12-15 19:37:24 -0300

    Add files via upload

---
+++ Questão 2.php
@@ -0,0 +1,11 @@
+<?php
+$dados_pessoa = [
+   "Nome: Maria Clara",
+   "CPF: 087883456-18",
+   "RG: 2006089107117",
+   "Idade: 55",
+   "Altura: 1.75",
+];
+foreach ($dados_pessoa as $dado) {
+   echo $dado . "<br>";
+};
---
+++ Questão 4.php
@@ -0,0 +1,7 @@
+<?php
+   $nome = $_POST[nome];
+   $email = $_POST[e-mail];
+   $idade = $_POST[idade];
+   echo "Nome de Usuário: $nome"."<br>";
+   echo "Endereço de E-mail: $email"."<br>";
+   echo "Idade: $idade";
